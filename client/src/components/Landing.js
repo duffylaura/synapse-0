affb08f948206fdb28f804696c98729a08d817e3
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-04 16:38:24 -0500

    Was able to import google fonts and change font on buttons; not times new roman though, have to find command for that.

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,18 +1,26 @@
 import React from "react";
 import logo from '../assets/logo-grey-banner.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-const styles = {
-// add in breakpoint for logo 
-
-}
+import '../index.css';
 
 function Landing() {
     return (
-        <div>
-        <img src={logo} class="img-fluid" alt='Synapse Logo'></img>
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-sm"> </div>
+                <div class="col-sm">
+                    <img src={logo} class="img-fluid" alt='Synapse Logo'></img>
+                    <div>
+                        <div class="col text-center"> <button class="custom-button"> Login </button> </div> <br></br>
+                        <div class="col text-center"><button class="custom-button"> Sign Up </button></div> <br></br>
+                        <div class="col text-center"><button class="custom-button"> About </button></div> <br></br>
+                    </div>
+                </div>
+                <div class="col-sm"> </div>
+            </div>
         </div>
     );
+
 }
 
 export default Landing;
