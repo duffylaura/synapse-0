1c0633f1bc1c9b14338109694772de8317383572
Coffee Pawz <lauralynneduffy@gmail.com>
2022-12-04 15:50:48 -0500

    Was able to change title of page using helment

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,6 +2,11 @@ import React from "react";
 import logo from '../assets/logo-grey-banner.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const styles = {
+// add in breakpoint for logo 
+
+}
+
 function Landing() {
     return (
         <div>
