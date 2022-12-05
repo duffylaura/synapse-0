7c8434f847438f30f62058ef26b44b445b294920
Morgan Kelly <mkelly5380@gmail.com>
2022-12-05 16:41:08 -0600

    added test seed data

--- server/models/User.js
+++ server/models/User.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-    groups: [
+    group: [
     {
         type: Schema.Types.ObjectId,
         ref:"Group"
