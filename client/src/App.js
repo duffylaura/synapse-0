e9e57e6d12c8d98bc5cba4abec3c0c9b4ae8227a
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-12-10 12:11:24 -0800

    build working, content not loading with console errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,44 @@
 import React from "react";
-import Login from './components/Login.js';
 import Helmet from "react-helmet";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+import Landing from './pages/Landing';
+import Login from './pages/Login.js';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import Group from './pages/Group';
+import Network from './pages/Network';
+import Nav from './components/NavBar/index';
+import NoMatch from './pages/NoMatch';
+
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
@@ -13,8 +51,45 @@ function App() {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
     <link rel="stylesheet" href='./index.css' />
     </Helmet>
-
-    < Login />
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+        <Nav />
+        <Routes>
+        <Route 
+                path="/" 
+                element={<Landing />} 
+              />
+              <Route 
+                path="/login" 
+                element={<Login />} 
+              />
+              <Route 
+                path="/signup" 
+                element={<Signup />} 
+              />
+              <Route 
+                path="/profile" 
+                element={<Profile />} 
+              />
+              <Route 
+                path="/group" 
+                element={<Group />} 
+              />
+              <Route 
+                // path="/group/:id"
+                path="/network" 
+                element={<Network />} 
+              />
+              <Route 
+                path="*" 
+                element={<NoMatch />} 
+              />
+        </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
+    
 
     </>
   )
