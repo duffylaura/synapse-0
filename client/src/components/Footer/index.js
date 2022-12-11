0adb63007a3d0d6a1e802f26f23c7b120a0c4f19
Rebecca Overton <rebeccaoverton@Rebeccas-MacBook-Air.local>
2022-12-11 13:15:17 -0800

    build successful, landing page links added

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import Auth from "../../utils/auth";
+// import { Link } from "react-router-dom";
+
+function Footer() {
+    return (
+        <h1> Footer Test</h1>
+    )
+};
+
+export default Footer;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import Auth from "../../utils/auth";
+// import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <h1> Header Test</h1>
+    )
+};
+
+export default Header;
