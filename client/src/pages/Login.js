6fb76bf85d43d8f5fbe2080ea31425c96bdc3dbe
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-12-12 13:43:50 -0800

    generate the login page to connect with auth and mutation for username and password

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,61 +1,54 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
+import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
+
 
 // Here we import a helper function that will check if the email is valid
-import { checkPassword, validateEmail } from '../utils/helpers';
+// import { checkPassword, validateEmail } from '../utils/helpers';
 
 //Style
 import logo from '../assets/logo-grey-banner.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 
-function Login() {
+const Login = () => {
+  const [formState, setFormState] = useState({ 
+    username: '',
+    password: '' 
+  });
+  const [login, { error, data }] = useMutation(LOGIN);
 
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'userName') {
-      setUserName(inputValue);
-    } else {
-      setPassword(inputValue);
-    }
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
   };
 
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+      // clear form values
+      Auth.login(data.login.token);
+      setFormState({
+        username: '',
+        password: '',
+      });
+    } catch (e) {
+      console.error(e);
     }
-    if (!checkPassword(password)) {
-      setErrorMessage(
-        `Choose a more secure password for the account: ${userName}`
-      );
-      return;
-    }
-    alert(`Hello ${userName}`);
 
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setUserName('');
-    setPassword('');
-    setEmail('');
   };
 
     return (
@@ -73,25 +66,32 @@ function Login() {
                 </div>
                 <div className="col-6">
                     <div>
-                        <form className="form">
+                    <form onSubmit={handleFormSubmit}>
                             <div className="col text-center"><h2> Login </h2></div><br></br>
                             <div className="col text-center">
-                                <input value={userName} name="userName" 
-                                onChange={handleInputChange} type="text" 
+                                <input value={formState.username} name="username" 
+                                onChange={handleChange} type="text" 
                                 placeholder="Username"/>
                             </div><br></br>
                             <div className="col text-center">
-                                <input value={password} name="password" 
-                                onChange={handleInputChange} type="password" 
+                                <input value={formState.password} name="password" 
+                                onChange={handleChange} type="password" 
                                 placeholder="Password."/>
                             </div><br></br>
-                            <div className="col text-center">
-                                <button className="custom-button" type="button" onClick={handleFormSubmit}>Login</button>
-                            </div>
+                            <button
+                              className="btn btn-block btn-info"
+                              style={{ cursor: 'pointer' }}
+                              type="submit"
+                            >
+                              Submit
+                            </button>
                         </form>
-                        {errorMessage && ( <div> <p className="error-text">{errorMessage}</p> </div>)}
+                        
+                        {error && (
+                          <div className="my-3 p-3 bg-danger text-white">
+                            {error.message}
+                            </div>)}
                     </div>
-                </div>
                 <div className="col-1"></div>
             </div>
 
@@ -102,9 +102,9 @@ function Login() {
             </div>
 
         </div>
-    </div>
-);
-
-}
+   </div>
+   </div>
+    )};
+    
 
 export default Login;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,74 +1,116 @@
-import React from 'react'
-import profilePlaceholder from '../assets/profile-placeholder.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../index.css';
-import logo from '../assets/logo-grey-banner.png';
+// import React from 'react'; 
 
-class ProfileForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: 'Enter text here...'
-      };
+// import { Navigate, useParams } from 'react-router-dom';
+// import { useQuery } from '@apollo/client';
+
+// import { QUERY_SINGLE_USER, QUERY_ME } from '../utils/queries';
+
+
+// import profilePlaceholder from '../assets/profile-placeholder.png';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import '../index.css';
+// import logo from '../assets/logo-grey-banner.png';
+
+// const Profile = () => {
+//     const { profileId } = useParams();
   
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
+//     // If there is no `profileId` in the URL as a parameter, execute the `QUERY_ME` query instead for the logged in user's information
+//     const { loading, data } = useQuery(
+//       profileId ? QUERY_SINGLE_USER : QUERY_ME,
+//       {
+//         variables: { profileId: profileId },
+//       }
+//     );
+//       // Check if data is returning from the `QUERY_ME` query, then the `QUERY_SINGLE_PROFILE` query
+//   const profile = data?.me || data?.profile || {};
+
+//   // Use React Router's `<Navigate />` component to redirect to personal profile page if username is yours
+//   if (Auth.loggedIn() && Auth.getProfile().data._id === profileId) {
+//     return <Navigate to="/me" />;
+//   }
+
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (!profile?.name) {
+//     return (
+//       <h4>
+//         You need to be logged in to see your profile page. Use the navigation
+//         links above to sign up or log in!
+//       </h4>
+//     );
+//   }
   
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
+
+// // class ProfileForm extends React.Component {
+// //     constructor(props) {
+// //       super(props);
+// //       this.state = {
+// //         value: 'Enter text here...'
+// //       };
   
-    handleSubmit(event) {
-      alert('Proifle was updated ' + this.state.value);
-      event.preventDefault();
-    }}
+// //       this.handleChange = this.handleChange.bind(this);
+// //       this.handleSubmit = this.handleSubmit.bind(this);
+// //     }
+  
+// //     handleChange(event) {
+// //       this.setState({value: event.target.value});
+// //     }
+  
+// //     handleSubmit(event) {
+// //       alert('Proifle was updated ' + this.state.value);
+// //       event.preventDefault();
+// //     }}
 
 
-function Profile() {
-    return (
-        <div class="container">
-        <div class="font">
-            <div class="row">
-                <div class="row"><br></br><br></br></div>
-                <div class="col-1"></div>
-                <div class="col-4"> 
-                    <div class="hide-on-phone">
-                        <div class="col-sm"><br></br></div>
-                        <div class="col-sm"><img src={profilePlaceholder} class="img-fluid" alt='Profile Image'></img> </div>
-                        <div class="col-sm"><br></br></div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div>
-                        <div class="col text-center"><h2> @Username</h2></div><br></br>
-                        <div class="col text-center"><input type="text"/></div><br></br>
-                        <div class="col text-center"><input type="text"/></div><br></br>
-                        <div class="col text-center"><input type="text"/></div><br></br>
 
-                        <form onSubmit={this.handleSubmit}>
-                            <label> How do you define yourself? <textarea value={this.state.value} onChange={this.handleChange} /> </label>
-                            <label> Details to share... <textarea value={this.state.value} onChange={this.handleChange} /> </label>
-                            <label> Link to your LinkedIn: <textarea value={this.state.value} onChange={this.handleChange}/></label>
-                            <input type="submit" value="Submit" />
-                        </form>
+
+// function Profile() {
+//     return (
+//         <div class="container">
+//         <div class="font">
+//             <div class="row">
+//                 <div class="row"><br></br><br></br></div>
+//                 <div class="col-1"></div>
+//                 <div class="col-4"> 
+//                     <div class="hide-on-phone">
+//                         <div class="col-sm"><br></br></div>
+//                         <div class="col-sm"><img src={profilePlaceholder} class="img-fluid" alt='Profile Image'></img> </div>
+//                         <div class="col-sm"><br></br></div>
+//                     </div>
+//                 </div>
+//                 <div class="col-6">
+//                     <div>
+//                         <div class="col text-center"><h2> @Username</h2></div><br></br>
+//                         <div class="col text-center"><input type="text"/></div><br></br>
+//                         <div class="col text-center"><input type="text"/></div><br></br>
+//                         <div class="col text-center"><input type="text"/></div><br></br>
+
+//                         <form onSubmit={this.handleSubmit}>
+//                             <label> How do you define yourself? <textarea value={this.state.value} onChange={this.handleChange} /> </label>
+//                             <label> Details to share... <textarea value={this.state.value} onChange={this.handleChange} /> </label>
+//                             <label> Link to your LinkedIn: <textarea value={this.state.value} onChange={this.handleChange}/></label>
+//                             <input type="submit" value="Submit" />
+//                         </form>
                 
-                    </div>
-                </div>
-                <div class="col-1"></div>
-            </div>
+//                     </div>
+//                 </div>
+//                 <div class="col-1"></div>
+//             </div>
 
-            <div class="hide-on-desktop">
-                <div class="col-sm"><br></br></div>
-                <div class="col-sm"><img src={logo} class="img-fluid" alt='Synapse Logo'></img> </div>
-                <div class="col-sm"><br></br></div>
-            </div>
+//             <div class="hide-on-desktop">
+//                 <div class="col-sm"><br></br></div>
+//                 <div class="col-sm"><img src={logo} class="img-fluid" alt='Synapse Logo'></img> </div>
+//                 <div class="col-sm"><br></br></div>
+//             </div>
 
 
-        </div>
-    </div>
-    );
+//         </div>
+//     </div>
+//     );
 
-}
+// }
+// };
 
-export default Profile;
+// export default Profile;
